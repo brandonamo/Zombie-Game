1f1a4ff8b09afb25fc5584bf427549916c148b93
gary1999 <Gary.She8362@mds.network>
2019-11-01 14:46:30 +1300

    Added Target/Gun Script

--- Zombie Game/Assets/Scripts/BasicMoving.cs
+++ Zombie Game/Assets/Scripts/BasicMoving.cs
@@ -4,19 +4,37 @@ using UnityEngine;
 
 public class BasicMoving : MonoBehaviour
 {
-    public float MoveSpeed;
+    public float MoveSpeed = 5.0F;
     CharacterController character;
 
     // Start is called before the first frame update
     void Start()
     {
-        character = GetComponent<CharacterController>();
+        //character = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 delta = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        character.SimpleMove(delta * MoveSpeed);
+        //Vector3 delta = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        //character.SimpleMove(delta * MoveSpeed);
+
+        float xAxisMovement = Input.GetAxis("Horizontal") * MoveSpeed;
+        float zAxisMovement = Input.GetAxis("Vertical") * MoveSpeed;
+
+        xAxisMovement *= Time.deltaTime;
+        zAxisMovement *= Time.deltaTime;
+
+        transform.Translate(xAxisMovement, 0, zAxisMovement);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
     }
 }
---
+++ Zombie Game/Assets/Scripts/TargetScript.cs
@@ -0,0 +1,27 @@
+﻿//https://www.youtube.com/watch?v=THnivyG0Mvo
+
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0)
+        {
+            Die();
+        }
+
+
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+
+
+}
